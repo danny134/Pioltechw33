4947989becb2370f2a27938729b5367427490042
daniel aparicio <daniAparicio97@outlook.es>
2019-02-11 20:06:38 +0100

    Agregar archivos de proyecto.

---
+++ Pioltech/.vb
@@ -0,0 +1,7 @@
+﻿Public Class Class1
+    Public Function listaNombresBtn(ByRef i As Integer) As String
+        Dim nombre As String
+        nombre = ToString("btn" & i)
+        Return nombre
+    End Function
+End Class
